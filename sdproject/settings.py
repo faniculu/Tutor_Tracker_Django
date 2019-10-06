b20aa8ac04810052a5525bddfe8c94966ebc4d31
mklthabo <736277@students.wits.ac.za>
2019-10-06 16:27:31 +0200

    installed the accounts app

--- sdproject/settings.py
+++ sdproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'tutortracker.apps.TutortrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
