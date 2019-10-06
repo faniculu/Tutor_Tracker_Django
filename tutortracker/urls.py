48189c9a4a55ff07038eb3ae1702bd8be2ea0377
mklthabo <736277@students.wits.ac.za>
2019-10-06 13:12:38 +0200

    changed url pattern

--- tutortracker/urls.py
+++ tutortracker/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('/about', AboutPageView.as_view(), name = 'about'),
+    path('tutortracker/', HomePageView.as_view(), name='home'),
+    path('tutortracker/about/', AboutPageView.as_view(), name = 'about'),
 ]
