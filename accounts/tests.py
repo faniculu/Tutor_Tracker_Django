abcfab9e8d57534344d840af49b86cfd4f614547
mklthabo <736277@students.wits.ac.za>
2019-10-07 01:57:26 +0200

    testing the sign up form

--- accounts/tests.py
+++ accounts/tests.py
@@ -75,5 +75,13 @@ class SimpleTests(SimpleTestCase):
 		self.assertTemplateUsed(response, 'registration/password_reset_form.html')
 
 
+class SignupPageTest(TestCase):
+	
+	username = 'newuser'
+	email = 'newuser@email.com'
 
-
+	def test_signup_form(self):
+		new_user = get_user_model().objects.create_user(self.username, self.email)
+		self.assertEqual(get_user_model().objects.all().count(),1)
+		self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+		self.assertEqual(get_user_model().objects.all()[0].email, self.email)
