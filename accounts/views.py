d1783bad757294d3d5abc544a12e636a9a62f8cc
mklthabo <736277@students.wits.ac.za>
2019-10-06 19:27:22 +0200

    using the custom form now

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
 # Create your views here.
 class SignUpView(generic.CreateView):
-	form_class = UserCreationForm
+	form_class = CustomCreationForm
 	success_url = reverse_lazy('login')
 	template_name = 'signup.html'
