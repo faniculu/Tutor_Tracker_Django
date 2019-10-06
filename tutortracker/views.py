5b5924e83bdef5b7f4041cebba6dd1d53a46a0c5
mklthabo <736277@students.wits.ac.za>
2019-10-06 11:15:45 +0200

    used template view

--- tutortracker/views.py
+++ tutortracker/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class HomePageView(TemplateView):
+	template_name = 'home.html'
