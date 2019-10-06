b09f951e068224e2022865010b5eb1c27277206e
mklthabo <736277@students.wits.ac.za>
2019-10-06 16:21:28 +0200

    new test for login and logout

--- tutortracker/tests.py
+++ tutortracker/tests.py
@@ -11,3 +11,13 @@ class SimpleTests(SimpleTestCase):
 	def test_about_page_status_code(self):
 		response = self.client.get('/tutortracker/about/')
 		self.assertEqual(response.status_code,200)
+
+	def test_login_page_status_code(self):
+		response = self.client.get('/accounts/login/')
+		self.assertEqual(response.status_code,200)
+
+	def test_logout_page_status_code(self):
+		response = self.client.get('/accounts/logout/')\
+		#The logout url redirects to home page hence we check for 302 status code. 
+		#Read more about 302 status code https://en.wikipedia.org/wiki/HTTP_302
+		self.assertEqual(response.status_code,302)
