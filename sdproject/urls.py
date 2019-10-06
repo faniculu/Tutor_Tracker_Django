ca830f5725b6c1ae22887c5440f2e949e76f1513
mklthabo <736277@students.wits.ac.za>
2019-10-06 13:11:46 +0200

    changed url path

--- sdproject/urls.py
+++ sdproject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-	path('tutortracker',include('tutortracker.urls')),
     path('admin/', admin.site.urls),
+    path('',include('tutortracker.urls')),
 ]
