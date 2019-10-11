aff4e8bcfcc7936dd83d8a2f960e3630627411b6
mklthabo <736277@students.wits.ac.za>
2019-10-12 00:56:59 +0200

    new urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import SignUpView, UserUpdateView , UserDetailView
-
+from .views import (SignUp, UserUpdateView , UserDetailView ,StudentSignUpView , LectureSignUpView)
+from . import views
 urlpatterns = [
-	path('signup/',SignUpView.as_view() , name = 'signup'),
+	path('signup/', views.SignUp , name = 'signup'),
+	path('signup/student/',StudentSignUpView.as_view(), name = 'student_signup'),
+	path('signup/lecture/',LectureSignUpView.as_view(), name = 'lecture_signup'),
 	path('<int:pk>/update/',UserUpdateView.as_view(), name = 'update_user'),
 	path('<int:pk>/',UserDetailView.as_view(), name = 'user_details')
 ]
