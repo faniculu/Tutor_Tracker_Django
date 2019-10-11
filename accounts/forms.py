951aec7df1adda1719887d778a6a2c7f4f6c23ea
mklthabo <736277@students.wits.ac.za>
2019-10-12 00:56:04 +0200

    new model forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,22 +1,50 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.db import transaction
+from .models import CustomUser , Student
+from .choices import ACADEMIC_YEAR
 
-from .models import MyCustomUser
-
-class CustomCreationForm(UserCreationForm):
+#student
+class StudentSignUpForm(UserCreationForm):
+	student_number = forms.IntegerField(required = True, min_value = 100000)
+	academic_year = forms.ChoiceField(required = True, choices = ACADEMIC_YEAR, widget = forms.Select )
 
 	class Meta:
+		model = CustomUser
+		fields = UserCreationForm.Meta.fields + ('email',) 
 
-		model = MyCustomUser
-		fields = UserCreationForm.Meta.fields + ('email','stdno',) #adding the new field to the form
+	@transaction.atomic
+	def save(self):
+		user = super().save(commit=False)
+		user.is_student = True
+		user.save()
+		student = Student.objects.create(user=user, 
+			student_number = self.cleaned_data.get('student_number'), 
+			academic_year = self.cleaned_data.get('academic_year'))
+		return user
+		
 
+
+#lecture
+class LectureSignUpForm(UserCreationForm):
+	class Meta:
+		model = CustomUser
+		fields = UserCreationForm.Meta.fields + ('email',) 
+
+	def save(self, commit = True):
+		user = super().save(commit = False)
+		user.is_lecture = True
+		if commit:
+			user.save()
+		return user
+	
 
 
 class CustomChangeForm(UserChangeForm):
 
 	class Meta:
 
-		model = MyCustomUser
+		model = CustomUser
 		fields = UserChangeForm.Meta.fields
 
 
