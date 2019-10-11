eee52ce1221b292fb6b34671e4ecf07f5b8abf0a
mklthabo <736277@students.wits.ac.za>
2019-10-12 00:57:25 +0200

    changed user model

--- sdproject/settings.py
+++ sdproject/settings.py
@@ -122,5 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
-AUTH_USER_MODEL = 'accounts.MyCustomUser'
+AUTH_USER_MODEL = 'accounts.CustomUser'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
