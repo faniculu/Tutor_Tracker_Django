503ceefa015511ba6a1e7f60a9e6cd2ba774f169
mklthabo <736277@students.wits.ac.za>
2019-10-12 00:57:12 +0200

    new views

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
-from .forms import CustomCreationForm, CustomChangeForm
-from .models import MyCustomUser
+from .forms import StudentSignUpForm, LectureSignUpForm, CustomChangeForm
+from .models import CustomUser
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic import DetailView
 
 # Create your views here.
-class SignUpView(CreateView):
-	form_class = CustomCreationForm
+def SignUp(request):
+	return render(request , 'signup.html')
+
+class StudentSignUpView(CreateView):
+	form_class = StudentSignUpForm
 	success_url = reverse_lazy('login')
-	template_name = 'signup.html'
+	template_name = 'specific_signup.html'
+
+class LectureSignUpView(CreateView):
+	form_class = LectureSignUpForm
+	success_url = reverse_lazy('login')
+	template_name = 'specific_signup.html'
 
 class UserUpdateView(LoginRequiredMixin,UpdateView):
-	model = MyCustomUser
-	fields = ['username','email','first_name', 'last_name', 'stdno']
+	model = CustomUser
+	fields = ['username','email','first_name', 'last_name']
 	template_name = 'user_update_form.html'
 
 class UserDetailView(LoginRequiredMixin,DetailView):
-	model = MyCustomUser
+	model = CustomUser
 	template_name = 'user_details.html'
